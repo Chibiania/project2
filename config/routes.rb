4b90365c97b28a7f376f75c4f5126215939d6140
Toni Langley <chibia2@gmail.com>
2016-03-21 15:41:18 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+root to: "books#index"
+  resources :books do
+    resources :adaptations
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
