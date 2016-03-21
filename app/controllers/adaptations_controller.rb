75cfcd583ef880e14a1a059147227a42b3dfb2a8
Toni Langley <chibia2@gmail.com>
2016-03-21 17:50:45 -0400

    added index/show methods

--- app/controllers/adaptations_controller.rb
+++ app/controllers/adaptations_controller.rb
@@ -1,9 +1,11 @@
 class AdaptationsController < ApplicationController
 
   def index
+    @adaptations = Adaptation.all
   end
 
   def show
+    @adaptation = Adaptation.find(params[:id])
   end
 
   def new
