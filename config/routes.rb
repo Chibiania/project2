96ae3d647d60ae2b6286d0e82018c14348288bb0
Toni Langley <chibia2@gmail.com>
2016-03-22 14:23:37 -0400

    added users resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "books#index"
   resources :books
   resources :adaptations
-
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
