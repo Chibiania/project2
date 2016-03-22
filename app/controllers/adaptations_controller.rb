c966721779427bed7e6a4d6dfca55072e79b86e6
Toni Langley <chibia2@gmail.com>
2016-03-21 20:50:25 -0400

    added new/create/edit/update/destroy/strong params methods

--- app/controllers/adaptations_controller.rb
+++ app/controllers/adaptations_controller.rb
@@ -9,8 +9,32 @@ class AdaptationsController < ApplicationController
   end
 
   def new
+    @adaptation = Adaptation.new
+  end
+
+  def create
+    @adaptation = Adaptation.create(adaptation_params)
+    redirect_to adaptations_path
   end
 
   def edit
+    @adaptation = Adaptation.find(params[:id])
+  end
+
+  def update
+    @adaptation = Adaptation.find(params[:id])
+    @adaptation.update(adaptation_params)
+    redirect_to adaptation_path
+  end
+
+  def destroy
+    @adaptation = Adaptation.find(params[:id])
+    @adaptation.destroy
+    redirect_to adaptations_path
+  end
+
+  private
+  def adaptation_params
+    params.require(:adaptation).permit(:title, :date_of_release, :image_url, :comparison_to_book, :book_id)
   end
 end
