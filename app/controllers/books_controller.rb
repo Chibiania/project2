67659f62bfdd33776246289dc64c760adf9079ee
Toni Langley <chibia2@gmail.com>
2016-03-22 20:48:57 -0400

    added new/create/edit/update/destroy/book params methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,5 +7,39 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
+  def new
+    redirect_to root_path unless @current_user
+    @book = Book.new
+  end
+
+  def create
+    redirect_to root_path unless @current_user
+    @book = Book.create(book_params)
+    redirect_to books_path
+  end
+
+  def edit
+    redirect_to root_path unless @current_user
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    redirect_to root_path unless @current_user
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path
+  end
+
+  def destroy
+    redirect_to root_path unless @current_user
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :date_of_publication, :image_url, :description)
+  end
 end
