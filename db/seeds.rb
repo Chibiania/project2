6af917a0bf7db35e42f10eb91bb8643dda3c1b71
Toni Langley <chibia2@gmail.com>
2016-03-24 08:44:57 -0400

    add image for project

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,24 @@
 Book.destroy_all
 Adaptation.destroy_all
 
-book1 = Book.create(title: "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling", image_url: "mwah", date_of_publication: "your mum", description: "awesomesauce")
+book1 = Book.create(title: "Harry Potter and the Deathly Hallows", author: "J.K. Rowling", image_url: "hp7.jpg", date_of_publication: "2007", description: "The heart of Book 7 is a hero's mission--not just in Harry's quest for the Horcruxes, but in his journey from boy to man--and Harry faces more danger than that found in all six books combined, from the direct threat of the Death Eaters and you-know-who, to the subtle perils of losing faith in himself. Attentive readers would do well to remember Dumbledore's warning about making the choice between “what is right and what is easy,” and know that Rowling applies the same difficult principle to the conclusion of her series. While fans will find the answers to hotly speculated questions about Dumbledore, Snape, and you-know-who, it is a testament to Rowling's skill as a storyteller that even the most astute and careful reader will be taken by surprise.")
 
-book2 = Book.create(title: "Harry Potter and the Chamber of Secrets", author: "J.K. Rowling", image_url: "uhhog", date_of_publication: "steeples", description: "niiiiioce!")
+book2 = Book.create(title: "The Great Gatsby", author: "F. Scott Fitzgerald", image_url: "gatsbybook.jpg", date_of_publication: "1925", description: "The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted “gin was the national drink and sex the national obsession,” it is an exquisitely crafted tale of America in the 1920s.")
 
-book3= Book.create(title: "Harry Potter and the Prisoner of Azkaban", author: "J.K. Rowling", image_url: "mwah", date_of_publication: "your mum", description: "awesomesauce")
+book3= Book.create(title: "The Little Mermaid", author: "Hans Christian Andersen", image_url: "tlmbook.jpg", date_of_publication: "1837", description: "This is a fairy tale about a young mermaid willing to give up her life in the sea and her identity as a mermaid to gain a human soul.")
 
-book4 = Book.create(title: "Harry Potter and the Goblet of Fire", author: "J.K. Rowling", image_url: "uhhog", date_of_publication: "steeples", description: "niiiiioce!")
+book4 = Book.create(title: "A Game of Thrones", author: "George R. R. Martin", image_url: "gotbook.jpeg", date_of_publication: "1996", description: "Several noble houses fight a civil war over who should be king, while an exiled princess tries to find her place in the world, and the kingdom is threatened by some rising supernatural threat in the north.")
 
-book5= Book.create(title: "Harry Potter and the Order of the Phoenix", author: "J.K. Rowling", image_url: "mwah", date_of_publication: "your mum", description: "awesomesauce")
+adaptation1 = book1.adaptations.create(title: "Harry Potter and the Deathly Hallows Part One", date_of_release: "2010", image_url: "hp7p1.jpg", comparison_to_book: "THIS...IS...AWESOME!!!!!")
 
-book6 = Book.create(title: "Harry Potter and the Halfblood Prince", author: "J.K. Rowling", image_url: "uhhog", date_of_publication: "steeples", description: "niiiiioce!")
+adaptation2 = book1.adaptations.create(title: "Harry Potter and the Deathly Hallows Part Two", date_of_release: "2011", image_url: "hp7p2.jpg", comparison_to_book: "THIS...IS...AWESOME!!!!!")
 
-book7= Book.create(title: "Harry Potter and the Deathly Hallows", author: "J.K. Rowling", image_url: "mwah", date_of_publication: "your mum", description: "awesomesauce")
+adaptation3 = book2.adaptations.create(title: "The Great Gatsby", date_of_release: "1926", image_url: "gatsbyorig.jpg", comparison_to_book: "it was good")
 
-adaptation1 = book1.adaptations.create(title: "Harry Potter and the Sorcerer's Stone", date_of_release: "1999?", image_url: "ugh!", comparison_to_book: "they're soo cute!")
+adaptation4 = book2.adaptations.create(title: "The Great Gatsby", date_of_release: "2013", image_url: "gatsbyleo.jpeg", comparison_to_book: "it was good")
 
-adaptation2 = book2.adaptations.create(title: "Harry Potter and the Chamber of Secrets", date_of_release: "2001?", image_url: "morg!", comparison_to_book: "it's kinda dark")
+adaptation5 = book3.adaptations.create(title: "Hans Christian Andersen's The Little Mermaid", date_of_release: "1975", image_url: "tlm75.jpg", comparison_to_book: "it was good")
 
-adaptation3 = book3.adaptations.create(title: "Harry Potter and the Prisoner of Azkaban", date_of_release: "2002?", image_url: "yup", comparison_to_book: "buckbeak!")
+adaptation6 = book3.adaptations.create(title: "The Little Mermaid", date_of_release: "1989", image_url: "tlm89.jpg", comparison_to_book: "not good but not bad either")
 
-adaptation4 = book4.adaptations.create(title: "Harry Potter and the Goblet of Fire", date_of_release: "2004", image_url: "yup", comparison_to_book: "ferret boy")
-
-adaptation5 = book5.adaptations.create(title: "Harry Potter and the Order of the Phoenix", date_of_release: "2006", image_url: "morg!", comparison_to_book: "it's kinda dark")
-
-adaptation6 = book6.adaptations.create(title: "Harry Potter and the Halfblood Prince", date_of_release: "2008", image_url: "morg!", comparison_to_book: "it's pretty light considering")
-
-adaptation7 = book7.adaptations.create(title: "Harry Potter and the Deathly Hallows Part One", date_of_release: "2010", image_url: "morg!", comparison_to_book: "it's dark")
-
-adaptation8 = book7.adaptations.create(title: "Harry Potter and the Deathly Hallows Part Two", date_of_release: "2011", image_url: "morg!", comparison_to_book: "it's darker still")
+adaptation7 = book4.adaptations.create(title: "Game of Thrones (season 1)", date_of_release: "2011", image_url: "gotned.jpg", comparison_to_book: "THIS...IS...AWESOME!!!!!")
