b39876847510f1b45f2b60b7175c9391684796e4
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-25 22:33:00 -0400

    Added Robin's comments

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @books = Book.order("RANDOM()").limit(3)
+    # Didn't know you could do this!
     @adaptations = Adaptation.order("RANDOM()").limit(3)
   end
 end
